6f9dea79f0f9699432e92acec2352d2ce392ca57
yrajput <yuvrajrathore142@gmail.com>
2025-09-21 18:27:03 +0530

    Review Resume page ui complete

--- frontend/src/pages/ReviewResume.jsx
+++ frontend/src/pages/ReviewResume.jsx
@@ -1,8 +1,51 @@
+import { FileText, Sparkles } from 'lucide-react';
 import React from 'react'
-
+import { useState } from 'react';
 const ReviewResume = () => {
+  const [input, setInput] = useState('');
+    const handleOnSubmit = async (event) =>{
+        event.preventDefault();
+    }
   return (
-    <div>ReviewResume</div>
+    <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
+      <form onSubmit={handleOnSubmit} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
+        <div className='flex items-center gap-3'>
+          <Sparkles className='w-6 text-purple-500 ' />
+          <h1 className='text-xl font-semibold'>Review Resume</h1>
+        </div>
+        <p className='mt-6 text-sm font-semibold'>Upload Resume</p>
+
+        <input onChange={(event) => setInput(event.target.files[0])} accept='application/pdf' rows={4} className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600' required type='file' />
+
+          <p className='text-xs text-gray-500 font-semibold mt-1'>Support PDF resume only</p>
+
+        
+       
+        <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-br from-purple-500 to-pink-500 text-sm font-semibold text-white px-4 py-2 mt-6 rounded-lg cursor-pointer'>
+          <FileText className='w-5'/>
+          Review Resume
+        </button>
+      </form>
+
+      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]'>
+          <div className='flex items-center gap-3'>
+              <FileText className='w-5 h-5 text-purple-500'/>
+              <h1 className='text-xl font-semibold'>
+                  Analysis Resume
+              </h1>
+
+              
+          </div>
+          <div className='flex-1 flex justify-center items-center'>
+                  <div className='flex-1 flex justify-center items-center '>
+                    <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
+                      <FileText className='w-9 h-9 '/>
+                      <p>Upload a resume and click "Review Resume" to get started</p>
+                    </div>
+                  </div>
+              </div>
+      </div>
+    </div>
   )
 }
 
